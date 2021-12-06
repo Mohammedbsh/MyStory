ee77ed81b2d03a303fa63778e9c4963d2796f4fb
Mohammedbsh <91191695+Mohammedbsh@users.noreply.github.com>
2021-12-06 19:11:45 +0300

    SPlash Login

---
+++ app/src/main/java/com/example/mystory/splash.kt
@@ -0,0 +1,28 @@
+package com.example.mystory
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        moveToLoginScreen()
+
+    }
+    private fun moveToLoginScreen(){
+        // this code for codes Delayed
+        Handler(Looper.myLooper()!!).postDelayed({
+            finish()
+            val i = Intent(this,LoginActivtiy::class.java)
+            startActivity(i)
+        }, 3000)
+
+
+    }
+
+}
